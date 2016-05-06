1e77fe3f90ad9c8c48900001fd5901c0135352ee
masami <ma0001570@gmail.com>
2016-05-06 19:27:54 +0900

    change key bind for helm-for-files

--- init.el
+++ init.el
@@ -310,9 +310,9 @@
 (require 'helm-config)
 
 ;; key bindings
-;(global-set-key (kbd "C-x C-f") 'helm-for-files)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x b")   'helm-buffers-list)
+;(global-set-key (kbd "C-x b")   'helm-buffers-list)
+(global-set-key (kbd "C-x b") 'helm-for-files)
 (global-set-key (kbd "M-y")     'helm-show-kill-ring)
 (global-set-key (kbd "M-x")     'helm-M-x)
 

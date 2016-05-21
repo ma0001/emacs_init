b4a5eca75c72b64780b03bd6ef10f6aa4f95effa
ma <ma0001570@gmail.com>
2016-05-21 22:20:57 +0900

    use sary in eijiro

--- init.el
+++ init.el
@@ -432,7 +432,44 @@
 (autoload 'sdic-describe-word "sdic" "英単語の意味を調べる" t nil)
 (global-set-key "\C-cw" 'sdic-describe-word)
 
-(setq sdic-default-coding-system 'utf-8)
+(eval-after-load "sdic"
+  '(progn
+     (setq sdicf-array-command (executable-find "sary")) ; コマンドパス
+     (setq sdic-eiwa-dictionary-list
+           '((sdicf-client "~/.emacs.d/dicts/eijirou.sdic" (strategy array)))
+           sdic-waei-dictionary-list
+           '((sdicf-client "~/.emacs.d/dicts/waeijirou.sdic" (strategy array))))
+
+     ;; saryを直接使用できるように sdicf.el 内に定義されているarrayコマンド用関数を強制的に置換
+     (fset 'sdicf-array-init 'sdicf-common-init)
+     (fset 'sdicf-array-quit 'sdicf-common-quit)
+     (fset 'sdicf-array-search
+           (lambda (sdic pattern &optional case regexp)
+             (sdicf-array-init sdic)
+             (if regexp
+                 (signal 'sdicf-invalid-method '(regexp))
+               (save-excursion
+                 (set-buffer (sdicf-get-buffer sdic))
+                 (delete-region (point-min) (point-max))
+                 (apply 'sdicf-call-process
+                        sdicf-array-command
+                        (sdicf-get-coding-system sdic)
+                        nil t nil
+                        (if case
+                            (list "-i" pattern (sdicf-get-filename sdic))
+                          (list pattern (sdicf-get-filename sdic))))
+                 (goto-char (point-min))
+                 (let (entries)
+                   (while (not (eobp)) (sdicf-search-internal))
+                   (nreverse entries))))))
+
+     (defadvice sdic-forward-item (after sdic-forward-item-always-top activate)
+       (recenter 0))
+     (defadvice sdic-backward-item (after sdic-backward-item-always-top activate)
+       (recenter 0))))
+
+(setq sdic-default-coding-system 'utf-8-unix)
+
 
 ;; ----------------------------------------------------------------
 ;; searchweb

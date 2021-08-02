3b8134106ebf3ec8dc8b881307ae79c39f382060
ms.iwata <>
2021-08-03 08:54:18 +0900

    windows font set

--- my-init.el
+++ my-init.el
@@ -268,9 +268,13 @@
 ;; ----------------------------------------------------------------
 ;; font set
 ;; ----------------------------------------------------------------
-(custom-set-faces
- '(default ((t (:family "HackGenNerd" :foundry "nil" :slant normal :weight normal :height 141 :width normal)))))
- 
+(cond (system-darwin-p
+       (custom-set-faces
+	'(default ((t (:family "HackGenNerd" :foundry "nil" :slant normal :weight normal :height 141 :width normal))))))
+       (system-windows-p
+	(custom-set-faces
+	 '(default ((t (:family "HackGenNerd" :foundry "outline" :slant normal :weight normal :height 90 :width normal)))))))
+
 ;; Options->setdefault font で HackGenNerd を選択して Options->save options
 ;;
 ;; フォント
